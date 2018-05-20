562bd4b98942015631747661f498f7693bd5d989
Romain Durand <romaindurand89@gmail.com>
2018-05-20 06:30:39 +0200

    static build/serve for production

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.API_PORT || 5000;
+const api = express();
+const apiPort = process.env.API_PORT || 5000;
+const nodeEnv = process.env.NODE_ENV || 'dev'
 
-app.use(bodyParser.json());
-app.post('/api/new', (req, res) => {
+api.use(bodyParser.json());
+api.post('/api/new', (req, res) => {
   console.log(req.body)
   res.send({ express: 'Hello From Express' });
 });
-app.listen(port, () => console.log(`Listening on port ${port}`));
+api.listen(apiPort, () => console.log(`API listening on port ${apiPort}`));
+
+//serve build folder in production
+if (nodeEnv === 'production') {
+  const app = express();
+  const port = process.env.PORT || 3001
+  app.use(express.static('build'));
+  app.listen(port, () => console.log(`App server listening on port ${port}`))
+}
