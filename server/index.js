a74b76fef1dd26ff5d218d36d07c7ad50b0ce68d
Romain Durand <romaindurand89@gmail.com>
2018-05-21 03:15:59 +0200

    better uids, mongo auth

--- server/index.js
+++ server/index.js
@@ -1,31 +1,34 @@
 require('dotenv').config();
+const utils = require('./utils');
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const uid = require('uid-safe');
-const utils = require('./utils');
 
-const mongoUrl = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+const mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}` +
+  `@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}?authMechanism=DEFAULT&authSource=${process.env.MONGO_DATABASE}`;
+  
 const api = express();
 const apiPort = process.env.API_PORT || 5000;
 const nodeEnv = process.env.NODE_ENV || 'dev'
-
 api.use(bodyParser.json());
+
 api.post('/api/new', (req, res) => {
   if (!utils.isValidPoll(req.body)) return res.status(400).json({error: `invalid.payload`, payload: req.body});
   // TODO: create an interface for mongo
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, async (err, client) => {
+    if (err) throw err;
     const polls = client.db(process.env.MONGO_DATABASE).collection('polls');
-    // TODO: check if uid exists in db
-    const newUid = await uid(8);
+    const newUid = await utils.getUid(polls);
     await polls.insertOne({
       question: req.body.question,
       options: req.body.options,
       uid: newUid
     });
-    res.send({ pollId: newUid });
+    res.send(await polls.findOne({uid: newUid}));
+    client.close();
   });
 });
+
 api.listen(apiPort, () => console.log(`API listening on port ${apiPort}`));
 
 //serve build folder in production
--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,5 @@
+const uid = require('randomstring');
+
 module.exports = {
   isValidPoll (poll) {
     return this.isValidQuestion(poll.question) && this.isValidOptions(poll.options);
@@ -7,5 +9,14 @@ module.exports = {
   },
   isValidOptions (options) {
     return Array.isArray(options) && options.every(option => typeof option === 'string' && option.length <= 280)
+  },
+  async getUid (collection) {
+    const newUid = await uid.generate({
+      length: 5,
+      charset: 'alphanumeric'
+    });
+    const uidExists = await collection.findOne({uid: newUid});
+    if (uidExists) return this.getUid(collection);
+    return newUid;
   }
 }
