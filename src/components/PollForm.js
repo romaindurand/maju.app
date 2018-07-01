b099328aa1945225674e06596fde7d946e7770fe
Romain Durand <romaindurand89@gmail.com>
2018-07-01 22:26:24 +0200

    use recaptcha to validate poll form

--- src/components/PollForm.js
+++ src/components/PollForm.js
@@ -1,38 +1,58 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import Recaptcha from 'react-google-invisible-recaptcha';
 
 class PollForm extends Component {
   constructor() {
     super()
     this.state = {
       error: null,
-      options: ['', '', '']
+      question: '',
+      options: ['', '', ''],
+      recaptchaSiteKey: null
     }
   }
 
+  notifyError(errorMEssage, timeout) {
+    this.setState({error: errorMEssage})
+    this.errorTimeout = window.setTimeout(() => {
+      this.setState({error: null});
+    }, timeout);
+  }
+
+  getOptions() {
+    return this.state.options
+      .filter(option => option !== '')
+      .reduce((memo, option) =>
+        memo.includes(option) ? memo : [...memo, option], [])
+  }
+  
   async handleSubmit(event) {
     event.preventDefault();
-    const inputs = event.target.getElementsByTagName('input');
-    const formValues = Array.from(inputs).reduce((memo, input) => {
-      if (input.name ==='question') memo.question = input.value
-      else if (input.value !== '') memo.options.push(input.value)
-      return memo
-    }, {question: '', options: []});
+    if (this.getOptions().length < 2)
+    return this.notifyError('Give two different options or more.', 5000)
+    
+    this.recaptcha.execute()
+  }
+  
+  async postFormData () {
+    const token = this.recaptcha.getResponse()
     const response = await fetch('/api/new', {
       method: 'POST',
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(formValues)
+      body: JSON.stringify({
+        question: this.state.question,
+        options: this.getOptions(),
+        token
+      })
     });
     const body = await response.json();
-    if (body.error) {
-      this.setState({error: body.error})
-      this.errorTimeout = window.setTimeout(() => {
-        this.setState({error: null});
-      }, 5000);
-    } else {
-      this.props.history.push(`/${body.uid}`)
+    if (response.status !== 200) {
+      return this.notifyError(body.message, 5000)
     }
+    this.props.history.push(`/${body.uid}`)
   }
+
   updateOption(index, event) {
     const options = this.state.options.slice()
     options[index] = event.target.value
@@ -43,30 +63,45 @@ class PollForm extends Component {
     window.clearTimeout(this.errorTimeout)
   }
 
+  async componentDidMount() {
+    const response = await fetch('/api/recaptcha')
+    const data = await response.json()
+    this.setState({recaptchaSiteKey: data.siteKey})
+  }
+
   render() {
     const optionInputList = this.state.options.map((option, index) => this.renderOption(index))
     return (
       <form className="new-poll-form" onSubmit={this.handleSubmit.bind(this)}>
         <h2>Create your <i>majority judgement</i> poll in seconds !</h2>
         <input
+          onChange={event => this.setState({ question: event.target.value })}
           autoFocus
           type="text"
           placeholder="Type your question here…"
-          name="question"
           className="drop"
           autoComplete="off"/>
         <h2>Add your poll options here</h2>
         <ol>{optionInputList}</ol>
         <div className="submit-container">
           <button className={this.state.error ? 'error' : ''}>Create poll</button>
-          {this.state.error ? <div className="error">Give at least two options</div> : ''}
+          {this.state.error ? <div className="error">{this.state.error}</div> : ''}
           <div style={{clear: 'both'}}></div>
         </div>
+        {
+          this.state.recaptchaSiteKey ?
+            <Recaptcha
+              ref={ ref => this.recaptcha = ref }
+              sitekey={ this.state.recaptchaSiteKey }
+              onResolved={ this.postFormData.bind(this) } />  
+            : null
+        }
       </form>
     );
   }
   
   handleOptionClick(i) {
+    this.setState({ error: null })
     if (i === this.state.options.length - 1) {
       this.setState({options: this.state.options.slice().concat([''])})
     }
@@ -78,7 +113,6 @@ class PollForm extends Component {
           autoComplete="off"
           value={this.state.options[i]}
           type="text"
-          name={`option${i}`}
           placeholder="Poll option…"
           onChange={event => this.updateOption(i, event)}
           onFocus={event => this.handleOptionClick(i)}/>
